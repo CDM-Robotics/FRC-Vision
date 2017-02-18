1d085aa8c15e8bf9ac9e65fc71f4918d30b864f4
rshortsma <1tolsmar@gmail.com>
2017-02-18 11:24:54 -0800

    lowered threshold value

--- image.py
+++ image.py
@@ -23,7 +23,7 @@ lab = cv2.cvtColor( cv2.GaussianBlur(image, (5,5),0 ), cv2.COLOR_BGR2LAB) #used 
 
 # threshold the image to reveal light regions in the
 # blurred image
-thresh = cv2.threshold(blurred, 220, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]
 
 # perform a series of erosions and dilations to remove
 # any small blobs of noise from the thresholded image
